a015738c29671172e6c485751042b436d5fa2ef9
Cullen S <cullensmi@gmail.com>
2020-11-13 18:23:54 -0500

    Finished main method to get every conditional in yahtzee, adjusted the number generators in the dice class to just generate numbers 1-6 randomly.

--- src/com/csmithswim/Dice.java
+++ src/com/csmithswim/Dice.java
@@ -1,40 +1,58 @@
 package com.csmithswim;
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Dice {
-    int counter = 2;
+    int counter = 3;
     ArrayList<Integer> currentRoll = new ArrayList<Integer>();
+    ArrayList<Integer> diceKept = new ArrayList<Integer>();
 
-    public void removeSelectedDice(String diceToBeRemoved){
+    public void keepSelectedDice(String diceToBeRemoved){
         String[] splitDice = diceToBeRemoved.split("");
-        System.out.println(Arrays.toString(splitDice));
 
-       for (String dice : splitDice) {
+            for (String dice : splitDice) {
+                diceKept.add(currentRoll.remove(Integer.parseInt(dice)));
+                currentRoll.add(Integer.parseInt(dice),Integer.parseInt(dice));
+            }
 
-           currentRoll.remove(Integer.parseInt(dice));
-           System.out.println(currentRoll);
+    currentRoll = diceKept;
+        System.out.println(currentRoll);
+        System.out.println(diceKept);
 
-       }
-                System.out.println(currentRoll);
+        System.out.println("\nThese are the dice you have kept:\n\n"+diceKept+".");
+
     }
 
-    public void rollSelectedDice(int numberOfDice){
+    public void rollSelectedDice(){
+//        System.out.println(currentRoll.size());
+//        System.out.println(currentRoll);
 
+        ArrayList<Integer> roll = new ArrayList<Integer>();
+        for (int i = 0; i <= 4-currentRoll.size(); i++) {
+            double rand = Math.ceil(Math.random() * 6);
+            int iRand;
+            iRand = (int) rand;
+            roll.add(iRand);
+        }
+        System.out.println("\nThese are the dice you have put aside:\n"+currentRoll);
+        System.out.println("\n   Dice \n"+roll);
+        System.out.println("--------------\n 0  1  2  3  4");
+        System.out.println("\nYou have " +(counter-1) + " roll(s) left.\n");
+        counter--;
+        currentRoll = roll;
     }
 
     public void rollAllDice() {
         ArrayList<Integer> roll = new ArrayList<Integer>();
         for (int i = 0; i <= 4; i++) {
-            double rand = Math.random() * 5;
+            double rand = Math.ceil(Math.random() * 6);
             int iRand;
             iRand = (int) rand;
             roll.add(iRand);
         }
-        System.out.println(roll + "\n--------------\n 0  1  2  3  4");
-        System.out.println("You have " + Integer.toString(counter) + " throws left.");
+        System.out.println("\n\n    Dice    \n"+roll + "\n--------------\n 0  1  2  3  4");
+        System.out.println("\nYou have " + (counter-1) + " rolls left.");
         counter--;
         currentRoll = roll;
     }
